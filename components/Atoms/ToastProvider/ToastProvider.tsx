f33bc4a24d6e8f0eb3ec9bca0fcf8ca8ea40e97e
jovana-rakocevic <jovana.rakocevic@vegait.rs>
2023-10-14 12:40:20 +0200

    Create a client provider for toast

---
+++ components/Atoms/ToastProvider/ToastProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+interface ToastProviderProps {
+  children: React.ReactNode;
+}
+
+export default function ToastProvider({ children }: ToastProviderProps) {
+  return (
+    <>
+      {children}
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
